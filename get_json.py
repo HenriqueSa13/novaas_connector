ddabf4435eb4874dbfe0a80d581bd92190f68622
Henrique <he.sa@campus.fct.unl.pt>
2023-04-07 18:02:47 +0100

    + write propertylink and evt

--- get_json.py
+++ get_json.py
@@ -6,7 +6,7 @@ data = json.load(f)
 
 evt_list = list()
 n_properties = 0
-
+n_properties_in_flow = 0
 #check aas id
 aas_id = data["assetAdministrationShells"][0]["identification"]["id"]
 print(aas_id)
@@ -22,8 +22,51 @@ for i in range(0,len(aas_opdata)) :
     if aas_opdata[i]["modelType"]["name"] == "BasicEvent":
         evt_list.append(aas_opdata[i])
 
-
-print(json.dumps(evt_list,indent=2))
+#print(json.dumps(evt_list,indent=2))
 #print(len(aas_opdata))
    
 f.close()
+
+with open('flow_opdata_todo.json', 'r') as f_flow:
+
+    flow_data = json.load(f_flow)
+    
+    for i in range(0,len(flow_data)):   
+        if "name" in flow_data[i].keys():
+            if flow_data[i]["name"] == "Property handler":
+                propertyhandler_id = flow_data[i]["id"]
+                print(flow_data[i]["id"])
+                break
+
+    for i in range(0,len(flow_data)):
+        
+        if "type" in flow_data[i].keys():
+
+            if flow_data[i]["type"] == ("subflow:" + propertyhandler_id) and ("Property " in flow_data[i]["name"]):
+                n_properties_in_flow += 1
+                
+
+    if n_properties != n_properties_in_flow:
+        print("WARNING")
+        print("Number of properties mismatch: \n properties in submodel - " + str(n_properties) + "\n properties in flow - " + str(n_properties_in_flow))
+        #exit() 
+
+    print(flow_data[46]["env"])
+    propertylink_dict = {}
+    propertylink_dict["name"] = "PropertyLink"
+    propertylink_dict["value"] = aas_id + "/OperationalData/" + evt_list[0]["observed"]["keys"][2]["value"]
+    propertylink_dict["type"] = "str"
+
+    propertylinkevt_dict = {}
+    propertylinkevt_dict["name"] = "PropertyLinkEvt"
+    propertylinkevt_dict["value"] = aas_id + "/OperationalData/" + evt_list[0]["idShort"]
+    propertylinkevt_dict["type"] = "str"
+
+    #print(evt_list[0]["idShort"])
+    print(propertylink_dict)
+    #flow_data[46]["env"].insert(2,)
+
+"""with open('flow_opdata_todo.json', 'w') as f_flow:
+    json.dump(flow_data, f_flow)"""
+
+
