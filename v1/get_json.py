a19d355baa2c87e40df0234b81e2f7f829bd0a01
Henrique <he.sa@campus.fct.unl.pt>
2023-05-14 21:04:59 +0100

    v1 complete (python)

--- v1/get_json.py
+++ v1/get_json.py
@@ -15,9 +15,8 @@ data = json.load(f)
 
 coll_list = list()
 evt_list = list()
-property_list = list()
 evt_name = ""
-n_properties = 0
+n_properties_w_event = 0
 n_properties_in_flow = 0
 
 #check aas id
@@ -33,9 +32,6 @@ for i in range(0,len(aas_opdata)) :
     if aas_opdata[i]["modelType"]["name"] == "SubmodelElementCollection":
         coll_list.append(aas_opdata[i]["value"])
 
-    if aas_opdata[i]["modelType"]["name"] == "Property":
-        n_properties += 1
-
     if aas_opdata[i]["modelType"]["name"] == "BasicEvent":
         for j in range(2,len(aas_opdata[i]["observed"]["keys"])):
             if j == len(aas_opdata[i]["observed"]["keys"]) - 1:
@@ -43,16 +39,9 @@ for i in range(0,len(aas_opdata)) :
             else:
                 evt_name += aas_opdata[i]["observed"]["keys"][j]["value"] + "."
         
+        n_properties_w_event +=1
         evt_list.append(evt_name) 
-        evt_name = ""
-        
-
-#print(json.dumps(evt_list,indent=2))
-#print(len(aas_opdata))
-
-#databases
-
-#print(len(coll_list[0]))
+        evt_name = ""    
 
 while len(coll_list) != 0:
     initial_len = len(coll_list)
@@ -68,14 +57,14 @@ while len(coll_list) != 0:
                         evt_name += coll_list[i][j]["observed"]["keys"][k]["value"] + "Evt"
                     else:
                         evt_name += coll_list[i][j]["observed"]["keys"][k]["value"] + "."
-                    
+                
+                n_properties_w_event +=1
                 evt_list.append(evt_name) 
                 evt_name = ""
  
     del coll_list[:initial_len]
           
-print(evt_list)      
-
+print(evt_list)
 f.close()
 
 
@@ -109,12 +98,12 @@ with open(filepath_flow, 'r') as f_flow:
                 property_dict = {}
                 property_dict["name"] = "PropertyName"
                 property_dict["type"] = "str"
-                property_dict["value"] = evt_list[j]["observed"]["keys"][2]["value"]
+                property_dict["value"] = evt_list[j].replace("Evt","")
                 
                 propertylink_dict = {}
                 propertylink_dict["name"] = "PropertyLink"
                 propertylink_dict["type"] = "str"
-                propertylink_dict["value"] = aas_id + "/OperationalData/" + evt_list[j]["observed"]["keys"][2]["value"]
+                propertylink_dict["value"] = aas_id + "/OperationalData/" + evt_list[j].replace("Evt","")
                 
                 historylength_dict = {}
                 historylength_dict["name"] = "HistoryLength"
@@ -124,7 +113,7 @@ with open(filepath_flow, 'r') as f_flow:
                 propertylinkevt_dict = {}
                 propertylinkevt_dict["name"] = "PropertyLinkEvt"
                 propertylinkevt_dict["type"] = "str"
-                propertylinkevt_dict["value"] = aas_id + "/OperationalData/" + evt_list[j]["idShort"]
+                propertylinkevt_dict["value"] = aas_id + "/OperationalData/" + evt_list[j]
                 
                 flow_data[i]["env"].insert(0,property_dict)
                 flow_data[i]["env"].insert(1,propertylink_dict)
@@ -138,9 +127,9 @@ with open(filepath_flow, 'r') as f_flow:
     prop submodel > flow - sao todas preenchidas no flow mas ficam em falta (possivel adicionar nos extra depois)
     prop submodel < flow - erro (evt list nao tem elementos suficientes - break do ciclo quando acabam elementos na lista?)
     """
-    if n_properties != n_properties_in_flow:
+    if n_properties_w_event != n_properties_in_flow:
         print("WARNING")
-        print("Number of properties mismatch: \n properties in submodel - " + str(n_properties) + "\n properties in flow - " + str(n_properties_in_flow))
+        print("Number of properties mismatch: \n properties in submodel - " + str(n_properties_w_event) + "\n properties in flow - " + str(n_properties_in_flow))
         #exit() 
 
 
